9efd61213e39b040877b7aa2fdeb665e90d4adfd
Hao079231 <haotrinh2003.com@gmail.com>
2025-05-04 17:11:42 +0700

    fix

--- src/main/java/vn/iotstar/demo/Model/Product.java
+++ src/main/java/vn/iotstar/demo/Model/Product.java
@@ -4,8 +4,6 @@ import vn.iotstar.demo.DAO.DBConnection;
 
 import org.springframework.beans.BeanUtils;
 
-import vn.iotstar.demo.DAO.DBConnection;
-
 /**
  * 
  */
@@ -85,115 +83,6 @@ public class Product {
     public int getId() {
         return id;
     }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Float getPrice() {
-        return price;
-    }
-
-    public void setPrice(Float price) {
-        this.price = price;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getBrandName() {
-        return brandName;
-    }
-
-    public void setBrandName(String brandName) {
-        this.brandName = brandName;
-    }
-
-    public String getIngredients() {
-        return ingredients;
-    }
-
-    public void setIngredients(String ingredients) {
-        this.ingredients = ingredients;
-    }
-
-    public String getOrigin() {
-        return origin;
-    }
-
-    public void setOrigin(String origin) {
-        this.origin = origin;
-    }
-
-    public Date getManufactureDate() {
-        return manufactureDate;
-    }
-
-    public void setManufactureDate(Date manufactureDate) {
-        this.manufactureDate = manufactureDate;
-    }
-
-    public Date getExpiryDate() {
-        return expiryDate;
-    }
-
-    public void setExpiryDate(Date expiryDate) {
-        this.expiryDate = expiryDate;
-    }
-
-    public Date getImportDate() {
-        return importDate;
-    }
-
-    public void setImportDate(Date importDate) {
-        this.importDate = importDate;
-    }
-
-    public String getUsageInstructions() {
-        return usageInstructions;
-    }
-
-    public void setUsageInstructions(String usageInstructions) {
-        this.usageInstructions = usageInstructions;
-    }
-
-    public Float getVolume() {
-        return volume;
-    }
-
-    public void setVolume(Float volume) {
-        this.volume = volume;
-    }
-
-    public String getProductImages() {
-        return productImages;
-    }
-
-    public void setProductImages(String productImages) {
-        this.productImages = productImages;
-    }
-
     /**
      * @param product  
      * @return
